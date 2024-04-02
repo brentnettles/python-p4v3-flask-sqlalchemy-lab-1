ed74e53a9bd94ae2e7b4a11f9a7d34b91f2fd75d
brentnettles <brentnettles@gmail.com>
2024-04-02 16:54:42 -0400

    completed lab / thanks Ben

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 # server/app.py
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
 from flask import Flask, make_response
 from flask_migrate import Migrate
@@ -20,7 +20,33 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+#adding views
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    eq = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if eq is None:
+        return make_response({'message': f'Earthquake {id} not found.'}, 404)
+
+    return make_response(eq.to_dict(), 200)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_mag(magnitude):
+    eqs = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    count = len(eqs)
+
+#mapped each Earthquake obj to dict
+    eq_dicts = []
+    for eq_obj in eqs:
+        eq_dicts.append(eq_obj.to_dict())
+
+    data = {
+        'count': count,
+        'quakes': eq_dicts
+    }
+
+    return make_response(data, 200)
+
 
 
 if __name__ == '__main__':
